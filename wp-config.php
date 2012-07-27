246b0e8289376a3b31e0390a015285dd5f84c568
iclanzan <iclanzan@macbook>
2012-07-27 12:33:11 +0300

    Entered database connection details.

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', $_ENV['OPENSHIFT_APP_NAME']);
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', $_ENV['OPENSHIFT_DB_USERNAME']);
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', $_ENV['OPENSHIFT_DB_PASSWORD']);
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', $_ENV['OPENSHIFT_DB_HOST']);
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
